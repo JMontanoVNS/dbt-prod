794a215c6be3359a07094264522d53c598b97ef4
JMontanoVNS <jose.montano@vnstudios.com>
2024-04-25 09:23:02 -0400

    Log run

--- extensions/product-discount/src/run.js
+++ extensions/product-discount/src/run.js
@@ -31,55 +31,13 @@ export function run(input) {
   const configuration = JSON.parse(
     input?.discountNode?.metafield?.value ?? "{}"
   );
+  console.error(configuration);
   if (!configuration.percentage && !configuration.fixedAmount) {
     return EMPTY_DISCOUNT;
   }
 
-  const targets = input.cart.lines
-    .filter((line) => {
-      if (line.merchandise.__typename == "ProductVariant") {
-        let hasTags = line.merchandise.product.hasAnyTag;
-        return hasTags === true;
-      }
-    })
-    .map((line) => {
-      const variant = /** @type {ProductVariant} */ (line.merchandise);
-      return /** @type {Target} */ ({
-        productVariant: {
-          id: variant.id,
-        },
-      });
-    });
-
-  if (!targets.length) {
-    console.error("No cart lines qualify for volume discount.");
-    return EMPTY_DISCOUNT;
-  }
-
-  let value = null;
-
-  if (Object.keys(configuration)[0] === "percentage") {
-    value = {
-      percentage: {
-        value: configuration.percentage.toString(),
-      },
-    };
-  } else {
-    value = {
-      fixedAmount: {
-        amount: configuration.fixedAmount.toString(),
-        appliesToEachItem: true
-      },
-    };
-  }
-
   return {
-    discounts: [
-      {
-        targets,
-        value,
-      },
-    ],
+    discounts: [],
     discountApplicationStrategy: DiscountApplicationStrategy.First,
   };
 }
