472dc1fd093c6e926145d2a678282f4d8a15ecd9
JMontanoVNS <jose.montano@vnstudios.com>
2024-04-30 17:03:06 -0400

    global is not defined error

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
+let global = { prisma: new PrismaClient() };
+
 const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-}
+// if (process.env.NODE_ENV !== "production") {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+// }
 
 export default prisma;
